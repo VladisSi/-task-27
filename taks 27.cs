ab546000b46d6881402bacaca13627cc4f037518
Kastalom22 <kastalom22@yandex.ru>
2023-10-08 09:22:27 +0300

    написан код к задачи 27

---
+++ taks 27.cs
@@ -0,0 +1,31 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        string input = Console.ReadLine();
+        
+        int sum = CalculateDigitSum(input);
+        
+        Console.WriteLine($"Сумма цифр в числе {input} равна {sum}");
+    }
+    
+    static int CalculateDigitSum(string input)
+    {
+        int sum = 0;
+        foreach (char digitChar in input)
+        {
+            if (char.IsDigit(digitChar))
+            {
+                int digit = (int)char.GetNumericValue(digitChar);
+                sum += digit;
+            }
+        }
+        return sum;
+    }
+}
